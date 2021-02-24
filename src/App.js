d487b106949afb7fc86399389c5aae192c795caf
ABHIJITBURMAN <abhijitburman7@gmail.com>
2021-02-24 17:23:36 +0530

    initial slider change and modal correction

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Button from './components/button'
 import Input from './components/input'
 import Label from './components/Label';
-
+import SliderComponent from './components/SliderComponent/imageSlider';
 function App() {
 
   const click = (e) => {
@@ -10,12 +10,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Hello</h1>
-      
-      <Input htmlId="username" name="name"/>
-      <Input htmlId="username" name="name"/>
-      <Button color='orange' type='reset' name="button1" onClick={(e)=> click()}>button1</Button>
-      <Button color='orange' type='reset' name="button1" onClick={(e)=> click()}>button2</Button>
+      <SliderComponent />
     </div>
   );
 }
---
+++ src/components/SliderComponent/imageSlider.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './sliderComponent.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+export default function imageSlider() {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 1,
+          partialVisibilityGutter: 30
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 1
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+    return (
+        <div className="sliderContainer">
+            <Carousel 
+            responsive={responsive}
+            focusOnSelect={true}
+            centerMode={true}
+            // showDots={true}
+            itemClass="carousel-item-padding-40-px"
+            itemClass="">
+            <div className="sliderItem"><img className="sliderPic" 
+              src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic"
+              src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic"
+              src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic"
+            src="https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80"            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic" 
+              src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic"
+              src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic"
+              src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+            />
+            </div>
+            <div className="sliderItem"><img className="sliderPic"
+            src="https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80"            />
+            </div>
+            </Carousel>;
+        </div>
+    )
+}
